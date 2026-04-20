09fb48bca71d292586c1e037ad7ba08d6914e4cc
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-04-20 22:04:55 +0500

    Properties Patterns

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -4,15 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Employee tom = new Employee();
-        Employee bob = new Manager();
+        Employee tom = new Employee("Tom", new Company("SilkRoad"));
+        Employee? bob = new Manager("Bob", new Company("Farovon"), true);
         UseEmployee(tom);
         UseEmployee(bob);
         
-        Employee sam = new Manager() { IsOnVacation = true };
-        Employee john = new Manager() { IsOnVacation = false };
-        EmployeeUsage(sam);
-        EmployeeUsage(john);
+        // Employee sam = new Manager() { IsOnVacation = true };
+        // Employee john = new Manager() { IsOnVacation = false };
+        // EmployeeUsage(sam);
+        // EmployeeUsage(john);
         // UseEmployee(tom);
         
         // Employee? bob = new Employee();
@@ -79,13 +79,24 @@ class Program
         }
         
         // Паттерн свойст
-        Person bakhtovar = new Person() { Language = "tajik", Status = "user", Name = "Bakhtovar"};
-        Person pierre = new Person() { Language = "french", Status = "user", Name = "Pierre" };
-        Person admin = new Person() { Language = "english", Status = "admin", Name = "Admin" };
+        Person bakhtovar = new Person { Language = "tajik", Status = "user", Name = "Bakhtovar"};
+        Person pierre = new Person { Language = "french", Status = "user", Name = "Pierre" };
+        Person admin = new Person { Language = "english", Status = "admin", Name = "Admin" };
+
+        Person tomas = new Person { Language = "german", Status = "admin", Name = "Tomas" };
+        Person pablo = new Person { Language = "spanish", Status = "user", Name = "Pablo" };
+
 
         SayHello(bakhtovar);
         SayHello(pierre);
         SayHello(admin);
+
+        Console.WriteLine();
+        
+        Console.WriteLine(GetMessage(tomas));
+        Console.WriteLine(GetMessage(pablo));
+        Console.WriteLine(GetMessage(pierre));
+        Console.WriteLine(GetMessage(null));
         
         void SayHello(Person person)
         {
@@ -96,16 +107,58 @@ class Program
             else
                 Console.WriteLine("Salom");
         }
+
+        string GetMessage(Person? p) => p switch
+        {
+            { Language: "english" } => "Hello!",
+            { Language: "german", Status: "admin" } => "Hallo, admin!",
+            { Language: "french", Name: var name } => $"Salut, {name}!",
+            { Language: var lang} => $"Unknown language: {lang}",
+            null => "null"
+        };
+        
+        var microsoft = new Company("Microsoft");
+        var google = new Company("Google");
+        var nick = new Employee("Nick", microsoft);
+        var ben = new Employee("Ben", google);
+        PrintCompany(nick);
+        PrintCompany(ben);
+
+        void PrintCompany(Employee employee)
+        {
+            if(employee is { Company.Title: "Microsoft" } )
+                Console.WriteLine($"{employee.Name} works in Microsoft");
+            else
+                Console.WriteLine($"{employee.Name} works somewhere");
+        }
     }
 }
 
 class Employee
 {
+    public string Name { get; }
+    public Company Company { get; set; }
+
+    public Employee(string name, Company company)
+    {
+        Name = name;
+        Company = company;
+    }
     public virtual void Work() => Console.WriteLine("Employee works.");
 }
 
+class Company
+{
+    public string Title { get; }
+    public Company(string title) => Title = title;
+}
 class Manager : Employee
 {
+    public Manager(string name, Company company, bool isOnVacation) : base(name, company)
+    {
+        IsOnVacation = isOnVacation;
+    }
+
     public override void Work() => Console.WriteLine("Manager works.");
     public bool IsOnVacation { get; set; }
 }

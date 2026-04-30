e8af80f62d6708086b51881c0d653caa2af7ea22
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-04-30 22:44:00 +0500

    Started learning Span<T>

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -511,6 +511,36 @@ class Program
         foreach (var person in employees123) Console.WriteLine(person);
         Console.WriteLine();
         
+        // ======== Span<T> ========
+        // 1st way
+        Span<string> people200 = ["Tom", "Bob", "Sam"];
+        
+        // 2nd way
+        string[] people2001 = { "Tom", "Bob", "Sam" };
+        Span<string> people202 = new Span<string>(people2001);
+        
+        // 3rd way
+        Span<string> people203 = people200;
+        
+        // We can word like arrays
+        
+        // Span Usings
+        int[] temperatures =
+        {
+            10, 12, 13, 14, 15, 11, 13, 15, 16, 17,
+            18, 16, 15, 16, 17, 14,  9,  8, 10, 11,
+            12, 14, 15, 15, 16, 15, 13, 12, 12, 11
+        };
+        Span<int> temperaturesSpan = temperatures;
+        Span<int> firstDecade = temperaturesSpan.Slice(0, 10);
+        Span<int> secondDecade = temperaturesSpan.Slice(20, 10);
+
+        temperaturesSpan[0] = 25;
+        
+        foreach (int decade in firstDecade) Console.Write(decade + " ");
+        Console.WriteLine();
+        foreach (int decade in secondDecade) Console.Write(decade + " ");
+
     }
 }
 

44db8bcc6c8b678be030fb0a7474354171a7fefd
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-05-10 21:28:41 +0500

    Substacting and formating

--- Times/Program.cs
+++ Times/Program.cs
@@ -9,8 +9,8 @@ class Program
 
         Console.WriteLine(DateTime.MinValue);
         
-        DateTime date1 = new DateTime(2006, 06, 26);
-        Console.WriteLine(date1);
+        DateTime myDate = new DateTime(2006, 06, 26);
+        Console.WriteLine(myDate);
         
         Console.WriteLine();
 
@@ -29,6 +29,24 @@ class Program
         DateTime date = new DateTime(2015, 7, 20, 18, 30, 25);
         Console.WriteLine(date.AddMinutes(45));
         Console.WriteLine();
+        
+        // Subtract methods
+        DateTime date1 = new DateTime(2015, 7, 20, 18, 30, 25);
+        DateTime date2 = new DateTime(2015, 7, 20, 15, 30, 25);
+        Console.WriteLine(date1.Subtract(date2));
+        Console.WriteLine();
+        
+        date1 = new DateTime(2015, 7, 20, 18, 30, 25);
+        Console.WriteLine(date1.AddHours(-3));
+        Console.WriteLine();
+        
+        // Date formating
+        Console.WriteLine(date1.ToLocalTime());
+        Console.WriteLine(date1.ToUniversalTime());
+        Console.WriteLine(date1.ToLongDateString());
+        Console.WriteLine(date1.ToShortDateString());
+        Console.WriteLine(date1.ToLongTimeString());
+        Console.WriteLine(date1.ToShortTimeString());
 
     }
 }

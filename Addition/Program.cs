e9ee24ae093d419a33922c8fecb819daa389ab51
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-05-11 21:46:26 +0500

    Lazy<T>

---
+++ Addition/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Addition;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Reader reader = new Reader();
+        reader.ReadEbooks();
+        reader.ReadBook();
+    }
+}
+
+class Reader
+{
+    Lazy<Library> library = new Lazy<Library>();
+    public void ReadBook()
+    {
+        library.Value.GetBook();
+        Console.WriteLine("Читаем бумажную книгу");
+    }
+
+    public void ReadEbooks()
+    {
+        Console.WriteLine("Читаем книгу на компьютере");
+    }
+}
+
+class Library
+{
+    string[] books = new string[99];
+
+    public void GetBook()
+    {
+        Console.WriteLine("Выдаем книгу читателю");
+    }
+}

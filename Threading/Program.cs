715ea0d968fabb91b8c7c9a3bc3a578c70ffa309
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-05-12 11:04:46 +0500

    Creating Threads

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -13,7 +13,7 @@ class Program
         Console.WriteLine($"Имя потока: {currentThread.Name}");
         currentThread.Name = "Метод Main";
         Console.WriteLine($"Имя потока: {currentThread.Name}");
-
+        
         Console.WriteLine($"Запущен ли поток: {currentThread.IsAlive}");
         Console.WriteLine($"Id потока: {currentThread.ManagedThreadId}");
         Console.WriteLine($"Приоритет потока: {currentThread.Priority}");
@@ -28,5 +28,32 @@ class Program
         }
         Console.WriteLine();
         
+        // ======== Creating threads. Delegate ThreadStart ========
+        Thread myThreads1 = new Thread(Print);
+        Thread myThreads2 = new Thread(new ThreadStart(Print));
+        Thread myThreads3 = new Thread(() => Console.WriteLine("Hello Threads"));
+
+        myThreads1.Start();
+        myThreads2.Start();
+        myThreads3.Start();
+        void Print() => Console.WriteLine("Hello Threads!");
+        Console.WriteLine();
+
+        Thread myThread = new Thread(Display);
+        myThread.Start();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Главный поток: {i}");
+            Thread.Sleep(300);
+        }
+        void Display()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Второй поток: {i}");
+                Thread.Sleep(400);
+            }
+        }
     }
 }

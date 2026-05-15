523892328d3dfd512fd3af650ed16c7e356aa259
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-05-15 23:09:57 +0500

    Type casting

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -75,5 +75,17 @@ class Program
         //
         //
         // void Show(object? message) => Console.WriteLine(message);
+
+        int number = 4;
+        Thread myThread = new Thread(Print);
+        myThread.Start(number);
+
+        void Print(object? obj)
+        {
+            if (obj is int n)
+            {
+                Console.WriteLine($"n * n = {n * n}");
+            }
+        }
     }
 }

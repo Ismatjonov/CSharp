0307a4ba9077247f11418c407f2769adc5fd7ae4
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-05-15 23:00:26 +0500

    Thread with parameters & ParameterizedThreadStart

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using System.Threading.Channels;
 
 namespace Threading;
 
@@ -8,26 +9,27 @@ class Program
     {
         // Getting current thread
         Thread currentThread = Thread.CurrentThread;
-        
+
         // getting thread's name
         Console.WriteLine($"Имя потока: {currentThread.Name}");
         currentThread.Name = "Метод Main";
         Console.WriteLine($"Имя потока: {currentThread.Name}");
-        
+
         Console.WriteLine($"Запущен ли поток: {currentThread.IsAlive}");
         Console.WriteLine($"Id потока: {currentThread.ManagedThreadId}");
         Console.WriteLine($"Приоритет потока: {currentThread.Priority}");
         Console.WriteLine($"Статус потока: {currentThread.ThreadState}");
         Console.WriteLine();
-        
+
         // use method Sleep()
         for (int i = 0; i < 10; i++)
         {
-            Thread.Sleep(500);  // delay execution by 500 milliseconds
+            Thread.Sleep(500); // delay execution by 500 milliseconds
             Console.WriteLine(i);
         }
+
         Console.WriteLine();
-        
+
         // ======== Creating threads. Delegate ThreadStart ========
         Thread myThreads1 = new Thread(Print);
         Thread myThreads2 = new Thread(new ThreadStart(Print));
@@ -47,6 +49,7 @@ class Program
             Console.WriteLine($"Главный поток: {i}");
             Thread.Sleep(300);
         }
+
         void Display()
         {
             for (int i = 0; i < 5; i++)
@@ -55,5 +58,22 @@ class Program
                 Thread.Sleep(400);
             }
         }
+        Console.WriteLine();
+        
+        
+        // ======== Threads with ParameterizedThreadStart ========
+        
+        // Creating new threads
+        Thread mt1 = new Thread(new ParameterizedThreadStart(Show));
+        Thread mt2 = new Thread(Show);
+        Thread mt3 = new Thread(message => Console.WriteLine(message));
+        
+        // Run threads
+        mt1.Start("Hello!");
+        mt2.Start("Привет!");
+        mt3.Start("Салом!");
+        
+        
+        void Show(object? message) => Console.WriteLine(message);
     }
 }

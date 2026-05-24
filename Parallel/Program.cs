4743262b71b6487098b035ca52ad137d30a5fbba
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-05-24 22:54:32 +0500

    Properties of class Task

--- Parallel/Program.cs
+++ Parallel/Program.cs
@@ -6,27 +6,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Task task1 = new Task(() => Console.WriteLine("Task1 is executed"));
-        task1.Start();
+        // Task task1 = new Task(() => Console.WriteLine("Task1 is executed"));
+        // task1.Start();
+        //
+        // Task task2 = Task.Factory.StartNew(() => Console.WriteLine("Task2 is executed"));
+        //
+        // Task task3 = Task.Run(() => Console.WriteLine("Task3 is executed"));
+        //
+        // task1.Wait();
+        // task2.Wait();
+        // task3.Wait();
+        //
+        // // Execute synchronously
+        // Console.WriteLine("MAin Starts");
+        //
+        // Task task = new Task(() =>
+        // {
+        //     Console.WriteLine("Task Starts");
+        //     Thread.Sleep(1000);
+        //     Console.WriteLine("Task Ends");
+        // });
+        // task.RunSynchronously();
+        // Console.WriteLine("Main Ends");
         
-        Task task2 = Task.Factory.StartNew(() => Console.WriteLine("Task2 is executed"));
-        
-        Task task3 = Task.Run(() => Console.WriteLine("Task3 is executed"));
-
-        task1.Wait();
-        task2.Wait();
-        task3.Wait();
-        
-        // 
-        Console.WriteLine("MAin Starts");
-
-        Task task = new Task(() =>
+        // Properties of class Task
+        Task tsk = new Task(() =>
         {
-            Console.WriteLine("Task Starts");
+            Console.WriteLine($"Task{Task.CurrentId} Starts");
             Thread.Sleep(1000);
-            Console.WriteLine("Task Ends");
+            Console.WriteLine($"Task{Task.CurrentId} Ends");
         });
-        task.RunSynchronously();
-        Console.WriteLine("Main Ends");
+        tsk.Start();
+
+        Console.WriteLine($"tsk ID: {tsk.Id}");
+        Console.WriteLine($"tsk is Completed: {tsk.IsCompleted}");
+        Console.WriteLine($"tsk Status: {tsk.Status}");
+        
+        tsk.Wait();
     }
 }

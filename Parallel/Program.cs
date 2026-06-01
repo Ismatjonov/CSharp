81d4c4543d53fd304b86cc77fdaaa918a6c9482a
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-06-01 16:08:09 +0500

    returning result from tasks

--- Parallel/Program.cs
+++ Parallel/Program.cs
@@ -85,5 +85,15 @@ class Program
         }
         Console.WriteLine("End of Main");
         Task.WaitAll(tasks);
+        
+        // returning results from tasks
+        int n1 = 4, n2 = 5;
+        Task<int> sumTask = new Task<int>(() => Sum(n1, n2));
+        sumTask.Start();
+        
+        int result = sumTask.Result;
+        Console.WriteLine($"{n1} + {n2} = {result}");
+        
+        int Sum(int a, int b) => a + b;
     }
 }

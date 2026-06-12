19b7c017785092e2f3e6d2be28529ba1e1a553ff
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-06-12 15:38:49 +0500

    method Task.Delay()

--- Asynchronous/Program.cs
+++ Asynchronous/Program.cs
@@ -4,20 +4,32 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        // await PrintAsync();
+        // Console.WriteLine("Some action in method Main");
+        //
+        // void Print()
+        // {
+        //     Thread.Sleep(3000);
+        //     Console.WriteLine("Hello METANIT.COM");
+        // }
+        //
+        // async Task PrintAsync()
+        // {
+        //     Console.WriteLine("Start PrintAsync");
+        //     await Task.Run(Print);
+        //     Console.WriteLine("End PrintAsync");
+        // }
+        
+        // Delay asynchronous operations and Task.Delay
         await PrintAsync();
-        Console.WriteLine("Some action in method Main");
-
-        void Print()
-        {
-            Thread.Sleep(3000);
-            Console.WriteLine("Hello METANIT.COM");
-        }
+        Console.WriteLine("Some actions in method Main");
 
         async Task PrintAsync()
         {
-            Console.WriteLine("Start PrintAsync");
-            await Task.Run(Print);
-            Console.WriteLine("End PrintAsync");
+            await Task.Delay(3000);
+            // or like this
+            // await Task.Delay(TimeSpan.FromSeconds(3));
+            Console.WriteLine("Hello Metanit.com");
         }
     }
 }

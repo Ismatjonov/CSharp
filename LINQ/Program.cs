3857e01b4b0ce276f8e764cc738a17f4ac4ffd64
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-06-16 12:33:12 +0500

    LINQ

---
+++ LINQ/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LINQ;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill" };
+        var selectedPeople = new List<string>();
+        
+        foreach(string person in people)
+            if (person.ToUpper().StartsWith("T"))
+                selectedPeople.Add(person);
+        
+        selectedPeople.Sort();
+        
+        foreach(string person in selectedPeople)
+            Console.WriteLine(person);
+        Console.WriteLine();
+        
+        // --- LINQ query operators ---
+        var linqSelectedPeople = from p in people
+            where p.ToUpper().StartsWith("T")
+            orderby p
+            select p;
+        
+        foreach(string person in linqSelectedPeople)
+            Console.WriteLine(person);
+        Console.WriteLine();
+        
+    }
+}

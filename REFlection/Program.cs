e1ec52139e50b584b6176939fbb3a40d0d1fa582
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-06-19 14:51:40 +0500

    Reflection: typeof, GetType() & Type.GetType()

---
+++ REFlection/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Reflection;
+
+namespace REFlection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Getting the type by typeog
+        Type myType = typeof(Person);
+
+        Console.WriteLine(myType.Name);
+        
+        // method GetType() : Object
+        Person tom = new("Tom", 27);
+        Type tomType = tom.GetType();
+        Console.WriteLine(tomType.Name);
+        
+        // static method Type.GetType()
+        Type? personType = Type.GetType("REFlection.Person", false, true);
+        Console.WriteLine($"type is {personType.Name}");
+        
+        // additional info
+        Type infoType = typeof(Person);
+
+        Console.WriteLine($"Name: {infoType.Name}");
+        Console.WriteLine($"Full Name: {infoType.FullName}");
+        Console.WriteLine($"Namespace: {infoType.Namespace}");
+        Console.WriteLine($"Is struct: {infoType.IsValueType}");
+        Console.WriteLine($"Is class: {infoType.IsClass}");
+
+        Console.WriteLine();
+        // Search for implemented interfaces
+        Console.WriteLine("Implemented interfaces:");
+        foreach(Type i in infoType.GetInterfaces())
+            Console.WriteLine(i.Name);
+        Console.WriteLine();
+        
+        // ========== Using reflection and the study of types ==========
+        foreach (MemberInfo mi in infoType.GetMembers())
+        {
+            Console.WriteLine($"{mi.DeclaringType} {mi.MemberType} {mi.Name}");
+        }
+
+        Console.WriteLine();
+        // --- BindingFlags ---
+        foreach (MemberInfo member in infoType.GetMembers(BindingFlags.DeclaredOnly | BindingFlags.Instance |
+                                                          BindingFlags.NonPublic | BindingFlags.Public))
+        {
+            Console.WriteLine($"{member.DeclaringType} {member.MemberType} {member.Name}");
+        }
+        Console.WriteLine();
+        
+        // --- Getting only one component by name [GetMember()] ---
+        MemberInfo[] print = infoType.GetMember("Print", BindingFlags.Instance | BindingFlags.Public);
+        foreach(MemberInfo mi in print)
+            Console.WriteLine($"{mi.MemberType} {mi.Name}");
+        Console.WriteLine();
+    }
+}
+
+class Person
+{
+    string name;
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        Age = age;
+    }
+    public void Print() => Console.WriteLine($"Name: {name}, Age: {Age}");
+}
+
+interface IEater
+{
+    void Eat();
+}
+
+interface IMovable
+{
+    void Move();
+}

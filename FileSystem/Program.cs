aa72f5bc58c494269651ab9e882430e56592fb3f
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-06-25 18:02:30 +0500

    Working with file system

---
+++ FileSystem/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FileSystem;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DriveInfo[] drives = DriveInfo.GetDrives();
+
+        foreach (DriveInfo drive in drives)
+        {
+            Console.WriteLine($"Name: {drive.Name}");
+            Console.WriteLine($"Type: {drive.DriveType}");
+            if (drive.IsReady)
+            {
+                Console.WriteLine($"Total Size: {drive.TotalSize}");
+                Console.WriteLine($"Free Space: {drive.TotalFreeSpace}");
+                Console.WriteLine($"Drive label: {drive.VolumeLabel}");
+            }
+        }
+    }
+}

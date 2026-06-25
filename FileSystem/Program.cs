f4124370923e4744e820957f026b0c500a29b23f
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-06-25 19:12:33 +0500

    IO: filtering, creating, getting info, deleting and moving

--- FileSystem/Program.cs
+++ FileSystem/Program.cs
@@ -23,7 +23,7 @@ class Program
         Console.WriteLine();
         
         // ========== Working with directories ========== 
-        string dirName = "C:\\Program Files";
+        string dirName = @"C:\Users\HOME\RiderProjects\Exercises\Exercises\bin\Debug\net9.0";
 
         if (Directory.Exists(dirName))
         {
@@ -40,6 +40,71 @@ class Program
             {
                 Console.WriteLine(s);
             }
+        }
+        
+        Console.WriteLine("\nFILTERING");
+        // ---- Filtering folders and files
+        string[] _dirs = Directory.GetDirectories(dirName, "books*.");
+        
+        var directory = new DirectoryInfo(dirName);
+        DirectoryInfo[] dirrs = directory.GetDirectories("books*.");
+        // getting all files started with books
+        
+        string[] _files = Directory.GetFiles(dirName, "*.exe");
+        
+        var _directory = new DirectoryInfo(dirName);
+        FileInfo[] files_ = _directory.GetFiles("*.exe");
+
+        foreach (var file in files_)
+        {
+            Console.WriteLine(file.Name);
+        }
+        
+        // ---- Creating some dir ----
+        // string path = @"C:\SomeDir";
+        // string subpath = @"program\avalon";
+        // DirectoryInfo dirInfo = new DirectoryInfo(path);
+        // if (!dirInfo.Exists)
+        // {
+        //     dirInfo.Create();
+        // }
+        // dirInfo.CreateSubdirectory(subpath);
+        // Console.WriteLine();
+        
+        // ---- getting information about dir ----
+        string dName = @"C:\Program Files";
+        
+        DirectoryInfo dinfo = new DirectoryInfo(dName);
+
+        Console.WriteLine($"Dir name: {dName}");
+        Console.WriteLine($"Full dir name: {dinfo.FullName}");
+        Console.WriteLine($"Creation time: {dinfo.CreationTime}");
+        Console.WriteLine($"Root directory: {dinfo.Root}");
+
+        Console.WriteLine();
+        
+        // ---- Deleting ----
+        /*string directName = @"C:\SomeDir";
+        if (Directory.Exists(directName))
+        {
+            Directory.Delete(directName);
+            Console.WriteLine("Directory deleted");
+        }
+        else
+        {
+            Console.WriteLine("Directory is not exists");
+        }*/
+        
+        // ---- Moving a directory ----
+        string oldPath = @"C:\SomeFolder";
+        string newPath = @"C:\SomeDir";
+        Directory.CreateDirectory(oldPath);
+        DirectoryInfo directoryInfo = new DirectoryInfo(oldPath);
+        if (directoryInfo.Exists && !Directory.Exists(newPath))
+        {
+            directoryInfo.MoveTo(newPath);
+            // or like this
+            // Directory.Move(oldPath, newPath);
         }
     }
 }

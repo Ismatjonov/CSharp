01cc5d64d99d07de85b9648e87024f07bede98fb
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-07-02 11:56:36 +0500

    Working with XML using class System.Xml

---
+++ XML/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml;
+
+namespace XML;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        XmlDocument xDoc = new XmlDocument();
+        xDoc.Load("C:\\Users\\HOME\\RiderProjects\\CSharp\\XML\\people.xml");
+        
+        // getting root element
+        XmlElement xRoot = xDoc.DocumentElement;
+        if (xRoot != null)
+        {
+            foreach (XmlElement xNode in xRoot)
+            {
+                XmlNode? attr = xNode.Attributes.GetNamedItem("name");
+                Console.WriteLine(attr?.Value);
+
+                foreach (XmlNode childNode in xNode.ChildNodes)
+                {
+                    if(childNode.Name == "company")
+                        Console.WriteLine($"Company: {childNode.InnerText}");
+                    if (childNode.Name == "age")
+                        Console.WriteLine($"Age: {childNode.InnerText}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

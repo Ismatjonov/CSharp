042e99544671be8fff6ec4114b76b6f237e04a1f
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-07-02 15:23:37 +0500

    Working with class and structs

--- XML/Program.cs
+++ XML/Program.cs
@@ -26,7 +26,41 @@ class Program
                         Console.WriteLine($"Age: {childNode.InnerText}");
                 }
                 Console.WriteLine();
+                
+                // ---- Working with class and structs ----
+                var people = new List<Person>();
+                
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load("C:\\Users\\HOME\\RiderProjects\\CSharp\\XML\\people.xml");
+
+                XmlElement? xmlRoot = xmlDoc.DocumentElement;
+                if (xmlRoot is not null)
+                {
+                    foreach (XmlElement xmlNode in xmlRoot)
+                    {
+                        Person? person = new Person();
+                        XmlNode? att = xmlNode.Attributes.GetNamedItem("name");
+                        person.Name = att?.Value;
+
+                        foreach (XmlNode childNode in xmlNode.ChildNodes)
+                        {
+                            if (childNode.Name == "company")
+                                person.Company = childNode.InnerText;
+                            if (childNode.Name == "age")
+                                person.Age = int.Parse(childNode.InnerText);
+                        }
+                        people.Add(person);
+                    }
+                    foreach(Person person in people)
+                        Console.WriteLine($"{person.Name} ({person.Company}) - {person.Age}");
+                }
             }
         }
     }
+}
+class Person
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public string? Company { get; set; }
 }

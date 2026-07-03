3de1dd6cebd5b8c7c812f2b9e8a375231512e84b
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-07-03 15:33:40 +0500

    Application domains

--- Processes/Program.cs
+++ Processes/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
+
 namespace Processes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var proceess = Process.GetCurrentProcess();
+        /*var proceess = Process.GetCurrentProcess();
         Console.WriteLine($"Id: {proceess.Id}");
         Console.WriteLine($"Name: {proceess.ProcessName}");
         Console.WriteLine($"Virtual Memory: {proceess.VirtualMemorySize64}");
@@ -41,12 +43,25 @@ class Program
         Console.WriteLine();
         
         // ------ Starting a new processes ------
-        Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+        // Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
         
         ProcessStartInfo procInfo = new ProcessStartInfo();
-        procInfo.FileName = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+        procInfo.FileName = @"C:\Program Files\WindowsApps\TelegramMessengerLLP.TelegramDesktop_6.8.2.0_x64__t4vj0pshhgkwm\Telegram.exe";
 
-        procInfo.Arguments = "http://bakhtovar-portfolio.web.app";
-        Process.Start(procInfo);
+        // procInfo.Arguments = "http://bakhtovar-portfolio.web.app";
+        Process.Start(procInfo);*/
+        
+        
+        // ==================== Application domains =====================
+        AppDomain domain = AppDomain.CurrentDomain;
+        Console.WriteLine($"Name: {domain.FriendlyName}");
+        Console.WriteLine($"Base Directory: {domain.BaseDirectory}");
+        Console.WriteLine();
+        
+        Assembly[] assembly = domain.GetAssemblies();
+        foreach (Assembly asm in assembly)
+        {
+            Console.WriteLine(asm.GetName().Name);
+        }
     }
 }

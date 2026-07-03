c2f49ae103e93bd7edc867b955d8d9c0ec8db5ed
Bakhtovar Ismatjonov <bakhtovar685@gmail.com>
2026-07-03 14:29:10 +0500

    Processes

---
+++ Processes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+namespace Processes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var proceess = Process.GetCurrentProcess();
+        Console.WriteLine($"Id: {proceess.Id}");
+        Console.WriteLine($"Name: {proceess.ProcessName}");
+        Console.WriteLine($"Virtual Memory: {proceess.VirtualMemorySize64}");
+        Console.WriteLine();
+        
+        // Getting all run processes
+        foreach (Process process in Process.GetProcesses())
+        {
+            // Console.WriteLine($"Id: {process.Id}, Name: {process.ProcessName}");
+        }
+
+        Console.WriteLine();
+        
+        // getting an ID of processes which introduce Rider
+        Process[] riderProcesses = Process.GetProcessesByName("telegram");
+        foreach(Process riderProcess in riderProcesses)
+            Console.WriteLine($"Id: {riderProcess.Id}");
+        
+        
+    }
+}
